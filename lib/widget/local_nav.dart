eb4c411097a2c82dbf15ab0d2edc01d117cbb300
SmartZ <247886052@qq.com>
2021-12-30 11:47:17 +0800

    创建 H5混合开发WebView组件

--- lib/widget/local_nav.dart
+++ lib/widget/local_nav.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_trip/model/common_model.dart';
 
+import 'hi_webview.dart';
+
 class LocalNav extends StatelessWidget {
   final List<CommonModel> localNavList;
 
@@ -34,7 +36,17 @@ class LocalNav extends StatelessWidget {
 
   Widget _item(BuildContext context, CommonModel model) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) =>
+                HiWebView(
+                  url: model.url,
+                  statusBarColor: model.statusBarColor,
+                  hideAppBar: model.hideAppBar,
+                )
+            )
+        );
+      },
       child: Column(
         children: [
           Image.network(
